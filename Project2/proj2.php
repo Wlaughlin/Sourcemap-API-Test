2aa7873e4cf672ef256f4a760fdc23b092bcec90
Will Laughlin <william.laughlin@trincoll.edu>
2012-07-05 17:17:04 -0400

    added space kitty

--- Project2/proj2.php
+++ Project2/proj2.php
@@ -1,18 +1,18 @@
-
 <html>
-<!--Html comments are dumb -->
-<head>
+   <body style="background-image:url('http://img.ffffound.com/static-data/assets/6/ffaf9fe485ebd5695dc2de3006c7d70f9435676b_m.jpg');>
+    <head>
         <title>project2</title>
-        <link rel="stylesheet" type="text/css" href="proj2.css">
-        
-    </head>
-
+    
+        <h1 style="font: comic-sans; color: green; background-color: grey;">This page is a BAMF</h1>
+    
+ </head>
+ <div style="background-color: white;">
+<script src="http://d3js.org/d3.v2.js"></script>
 
      <script type="text/javascript">
    function doThis() {
     var something = document.getElementById("usethis");
    var myurl = "http://sourcemap.com/services/supplychains/" + something.value;
-   alert(myurl);
           <?php 
         $link = "http://sourcemap.com/services/supplychains/" . $_GET["schain"];
     $myjsondata = file_get_contents($link); 
@@ -29,7 +29,10 @@
     Please enter the number of the supplychain you'd like to know about, hit enter, and then click submit: <input type = "text" name="schain" id="usethis" />
    <input type="submit" value="Submit" />
     </form>
-
+<div></div>
+<div style="float: bottom-right; background-color: white;">Space Kitty!</div>
+</div>
+    </body>
     
 </html>
 
